a06525aafaa3ff67f74d72d1f93a5438682e848b
ZMiaow <zmiaow@shu.edu.cn>
2026-09-08 12:42:23 +0800

    auto-backup: 2026-09-08 12:42:22 | source files

--- Main/Inc/app_config.h
+++ Main/Inc/app_config.h
@@ -7,9 +7,9 @@
 #define APP_ENABLE_MOVE_SPIN_TEST        0
 #define APP_ENABLE_AUTOMATIC_MOTOR_TEST  0
 #define APP_ENABLE_SERVO_SWEEP_TEST      0
-#define APP_ENABLE_TASK                  1
+#define APP_ENABLE_TASK                  0
 #define APP_ENABLE_CENTERING_TASK        0
-#define APP_ENABLE_MOTION_DEBUG_TASK     0
+#define APP_ENABLE_MOTION_DEBUG_TASK     1
 
 /* Non-blocking IMU angle turn used by Motor_TurnAngle(). */
 #define APP_MOTOR_TURN_TOLERANCE_MDEG    1000L
